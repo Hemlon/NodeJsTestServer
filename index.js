89f296d76172a0774b05e6251c344ac2f0d069ca
Hemlon <45729132+Hemlon@users.noreply.github.com>
2018-12-09 22:54:47 +1100

    Testing new game

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
+// Dependencies
+var express = require('express');
 var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
 });
 
+
 var port = process.env.PORT || 1337;
-server.listen(port);
+server.listen(port, function() {console.log('starting server on ' + port)});
 
 console.log("Server running at http://localhost:%d", port);
+
+io.on('connection', function(socket) {
+});
+
+setInterval(function() {
+	io.sockets.emit('message', 'hi!');
+}, 1000);
+
+
+var players = {};
+io.on('connection', function(socket) {
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+  });
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+});
+
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
+
+
---
+++ static/game.js
@@ -0,0 +1,61 @@
+var socket = io();
+socket.on('message', function(data) {
+	console.log(data);
+});
+
+var movement = {
+	up: false,
+	down: false,
+	left: false,
+	right: false
+}
+
+document.addEventListener('keydown', function(event) 
+{
+	var key = event.keyCode;
+	
+	if (key == 65)
+	{
+		movement.left = true;
+	}
+	else if (key == 87)
+	{
+		movement.up = true;
+	}
+	else if (key == 68)
+	{
+		movement.right = true;
+	}
+	else if (key == 83)
+	{
+		movement.down = true;
+	}
+}
+);
+
+socket.emit('new player');
+setInterval(function() {
+  socket.emit('movement', movement);
+}, 1000 / 60);
+
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+socket.on('state', function(players) {
+  context.clearRect(0, 0, 800, 600);
+  context.fillStyle = 'green';
+  for (var id in players) {
+    var player = players[id];
+    context.beginPath();
+    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    context.fill();
+  }
+});
+
+
+	
+	
+	
+}
+
