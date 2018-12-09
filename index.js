43ecbe5c2f091bf35bca1b8e2668ed7a9be0805f
Hemlon <45729132+Hemlon@users.noreply.github.com>
2018-12-10 01:05:03 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+// Dependencies
+var express = require('express');
 var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+//var server = http.Server(app);
 
 var server = http.createServer(function(request, response) {
 
@@ -7,7 +13,55 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = process.env.PORT || 1337;
+var portnumber = 1337;
+var io = socketIO(server);
+app.set('port', portnumber);
+
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+var port = process.env.PORT || portnumber;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Server running at %d", port);
+
+setInterval(function() {
+	io.sockets.emit('message', 'hi!');
+}, 1000);
+
+var players = {};
+
+io.on('connection', function(socket) {
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+  });
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+});
+
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
+
+
